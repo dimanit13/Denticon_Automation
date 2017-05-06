61766194b5c89dc6270d3930109f431a3fb932b4
Dinesh <dinesh.manit13@gmail.com>
2017-05-06 15:38:16 +0530

    removed hardcoded login property file path

--- denticon_advanced/src/test/java/com/planet/denticon/login/Login.java
+++ denticon_advanced/src/test/java/com/planet/denticon/login/Login.java
@@ -5,19 +5,21 @@ import com.planet.denticon.pom.ILogin;
 import com.planet.denticon.utilities.CommonMethods;
 
 public class Login {
-	String filePath="E:\\Selenium_Test\\denticon_advanced\\src\\main\\resources\\login.properties";
+	String filePath=System.getProperty("user.dir") + "\\src\\main\\resources\\login.properties";
 	String username="";
 	String password="";
 	Properties prop= new Properties();
 	@Test
 	public void login() {
 		try{
+			Thread.sleep(1000);
 			username=CommonMethods.readDatafromProps(filePath,"user");
 			password=CommonMethods.readDatafromProps(filePath,"password");
 			CommonMethods.sendText(ILogin.TXT_USERNAME, username);			
 			CommonMethods.sendText(ILogin.TXT_PASSWORD, password);
 			CommonMethods.clickIfEnable(ILogin.BTN_GO);			
 			Thread.sleep(3000);
+			
 		}
 		catch(Exception e){	  
 		}
