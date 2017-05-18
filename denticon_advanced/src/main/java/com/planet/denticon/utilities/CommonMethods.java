2bc7eb9d5f98920e0b4fd18299e8b98fc918413b
Dinesh <dinesh.manit13@gmail.com>
2017-05-18 08:53:25 +0530

    Added some new mwthods

--- denticon_advanced/src/main/java/com/planet/denticon/utilities/CommonMethods.java
+++ denticon_advanced/src/main/java/com/planet/denticon/utilities/CommonMethods.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
@@ -16,4 +17,114 @@ import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 
 
-public class CommonMethods {}
+
+public class CommonMethods {
+
+
+	static String url = "http://dev.denticon.com/aspx/home/login.aspx";
+
+
+	public static List<WebElement> getListelements(By lst){
+
+		List<WebElement> lstcommon= DriverManager.getDriver().findElements(lst);
+		if(lstcommon.isEmpty()){
+			return null;
+		}
+		else		
+			return lstcommon;
+	}
+	public static boolean clickIfEnable(By sElement){
+		boolean flag=false;
+		WebElement element=DriverManager.getDriver().findElement(sElement);
+		if(element.isEnabled()){
+			element.click();
+			flag=true;	
+		}
+		return flag;
+	}
+
+	public static boolean sendText(By loc, String text){
+		boolean flag=false;
+		WebElement element=DriverManager.getDriver().findElement(loc);
+		if(element!=null){
+			element.clear();
+			element.sendKeys(text);
+			flag=true;
+		}
+		return flag;
+
+	}
+
+	public static boolean selectFromList(By dropdwon, String seletedvalue){
+		boolean flag=false;
+		try{
+			WebElement element=DriverManager.getDriver().findElement(dropdwon);
+			if(element!=null){
+				new Select(element).selectByVisibleText(seletedvalue);
+				flag=true;
+			}
+		}catch(NoSuchElementException e){}
+		return flag;		
+	}
+	public static ExtentReports getReport(ExtentReports repo){
+		repo = new ExtentReports(System.getProperty("user.dir") + "\\report.html");
+		return repo;
+	}
+	public static void closeReport(ExtentReports repo, ExtentTest test ){
+		repo.endTest(test);
+		repo.flush();
+	}
+	public static String readDatafromProps(String filePath, String key)
+	{
+		File f=new File(filePath);
+		Properties prop= new Properties();
+		FileInputStream fileInput;
+		String value="";
+		try {				
+			fileInput = new FileInputStream(f);
+			prop.load(fileInput);
+			value=prop.getProperty(key);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return value;
+
+	}
+	public static boolean isElementPresent(By by) {
+		List<WebElement> elements = DriverManager.getDriver().findElements(by);
+		return !elements.isEmpty();
+
+	}
+	public static boolean listSelect(By lstPracArea, String lstValue) throws Exception {
+		boolean result = false;
+		WebElement element = getWebElement(lstPracArea);
+		if (element != null) {
+			new Select(element).selectByVisibleText(lstValue);
+			result = true;
+		}
+		Thread.sleep(3000);
+		return result;
+	}
+	public static WebElement getWebElement(By by) {
+		WebElement result = null;
+		List<WebElement> elements = DriverManager.getDriver().findElements(by);
+		if (!elements.isEmpty()) {
+			result = DriverManager.getDriver().findElement(by);
+		} else {
+			return null;
+		}
+		int attempts = 0;
+		while (attempts < 3) {
+			try {
+				result.isEnabled();
+				break;
+			} catch (StaleElementReferenceException e) {
+				result = DriverManager.getDriver().findElement(by);
+			}
+			attempts++;
+		}
+
+		return result;
+	}
+
+}
