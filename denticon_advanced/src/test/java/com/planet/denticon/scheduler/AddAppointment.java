a06c0e1ccca682dcf1f106f762a8674ad7421240
Dinesh <dinesh.manit13@gmail.com>
2017-05-21 11:13:10 +0530

    written add and edit apoointment

--- denticon_advanced/src/test/java/com/planet/denticon/scheduler/AddAppointment.java
+++ denticon_advanced/src/test/java/com/planet/denticon/scheduler/AddAppointment.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -21,9 +22,13 @@ import com.planet.denticon.utilities.CommonMethods;
 public class AddAppointment {
 	String stepInfo="";
 	boolean flag=false;
-	String date="18";
+	String date="24";
 	@Test
-	public void bookAppointment() {
+	public void appointments() {
+		//addAppointment();
+		editAppointment();
+	}
+	public void addAppointment(){
 		try {
 			stepInfo="Click on schedular tab";
 			flag=CommonMethods.clickIfEnable(IScheduler.TAB_SCHEDULER);
@@ -34,11 +39,17 @@ public class AddAppointment {
 			for (String window : windowHandles) {
 				DriverManager.getDriver().switchTo().window(window);
 			}
+
+			/*Thread.sleep(3000);
+			Alert alert=DriverManager.getDriver().switchTo().alert();
+			alert.accept();*/
 			DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
 			Date dateobj = new Date();
 			System.out.println(df.format(dateobj));
 			Thread.sleep(2000);
-				
+
+
+
 			stepInfo="Click on Calender Icon to select date";
 			Thread.sleep(2000);
 			flag=CommonMethods.clickIfEnable(IScheduler.ICON_CALENDER);
@@ -52,19 +63,83 @@ public class AddAppointment {
 					break;
 				}
 			}
+			Thread.sleep(3000);
+			stepInfo="select time for which you want to book an appointment";
+			List<WebElement> lst1=new ArrayList<WebElement>();
+			lst=DriverManager.getDriver().findElements(IScheduler.SLC_TIME_APPOINTMENT);
+			lst1.get(1).click();
+			//flag=CommonMethods.clickIfEnable(IScheduler.SLC_TIME_APPOINTMENT);
+			Thread.sleep(3000);
+			stepInfo="Click on Schedular tab";
+			flag=CommonMethods.clickIfEnable(IScheduler.TAB_SCHEDULER_DD);
 			
-			stepInfo="Right click on select time";
-			Thread.sleep(5000);
-			Actions action = new Actions(DriverManager.getDriver()).contextClick(DriverManager.getDriver().findElement(IScheduler.SLC_TIME_APPOINTMENT));
-			action.build().perform();
-			Thread.sleep(2000);
-			flag=CommonMethods.clickIfEnable(IScheduler.BTN_ADD_NEW_APPOINTMENT);
+			
+			
+		}
+		catch(Exception e){
+			
+		}
+		
+	}
+	public void editAppointment(){
 
-			
+		try {
+			stepInfo="Click on schedular tab";
+			flag=CommonMethods.clickIfEnable(IScheduler.TAB_SCHEDULER);
+			Thread.sleep(2000);
+			stepInfo="switch to new window";
+			String parentWindow=DriverManager.getDriver().getWindowHandle();
+			Set<String> windowHandles = DriverManager.getDriver().getWindowHandles();
+			for (String window : windowHandles) {
+				DriverManager.getDriver().switchTo().window(window);
+			}
+
+			/*Thread.sleep(3000);
+			Alert alert=DriverManager.getDriver().switchTo().alert();
+			alert.accept();*/
+			DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+			Date dateobj = new Date();
+			System.out.println(df.format(dateobj));
+			Thread.sleep(2000);
+
+
+
+			stepInfo="Click on Calender Icon to select date";
+			Thread.sleep(2000);
+			flag=CommonMethods.clickIfEnable(IScheduler.ICON_CALENDER);
+			stepInfo="select date ";
+			List<WebElement> lst=new ArrayList<WebElement>();
+			lst=DriverManager.getDriver().findElements(IScheduler.LST_DATES);
+			for(int i=0;i<lst.size();i++){
+				//System.out.println(lst.get(i).getText());
+				if(lst.get(i).getText().trim().equalsIgnoreCase(date)){
+					lst.get(i).click();
+					break;
+				}
+			}
+			//DriverManager.getDriver().findElement(By.id("aMenuPatient"));
+			//flag=CommonMethods.isElementPresent(IScheduler.SLC_TIME_APPOINTMENT);
+
+			stepInfo="Right click on select time";
+			Thread.sleep(8000);
+
+			Actions builder = new Actions(DriverManager.getDriver());
+			builder.contextClick(DriverManager.getDriver().findElement(IScheduler.TAB_BOOKEDAPP)).perform();
+			Thread.sleep(3000);
+			builder.contextClick(DriverManager.getDriver().findElement(IScheduler.TAB_BOOKEDAPP)).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+			Thread.sleep(3000);
+
+			//contextClick(DriverManager.getDriver().findElement(IScheduler.SLC_TIME_APPOINTMENT)).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+
+			Thread.sleep(5000);
+			//flag=CommonMethods.clickIfEnable(IScheduler.BTN_ADD_NEW_APPOINTMENT);
+
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		stepInfo="";
+	
 	}
 }
